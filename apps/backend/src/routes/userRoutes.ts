65ef780d3d06130ec31ce671a01205ce8f7197e7
Mahesh <maheshddivate@gmail.com>
2025-06-12 22:21:58 +0530

    feat: getUsers

--- apps/backend/src/routes/userRoutes.ts
+++ apps/backend/src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { handleFollowUser, handleUserSignin, handleUserSignup } from "../controllers/userControllers";
+import { handleFollowUser, handleUserSignin, handleUserSignup,handleGetUserLists } from "../controllers/userControllers";
 
 export const UserRouter : Router = Router();
 
 UserRouter.post("/user/signup", handleUserSignup as any);
 UserRouter.get("/user/signin",handleUserSignin as any)
 UserRouter.post("/user/signin",handleFollowUser as any)
+UserRouter.post("/user/:name",handleGetUserLists as any)
 
 
